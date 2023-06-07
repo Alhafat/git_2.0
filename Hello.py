e261beda78a1bd86d14d4bbacff01d1dc37aa340
Katerina <emrodionova@bk.ru>
2023-06-07 19:38:38 +0300

    some changes to task

--- Hello.py
+++ Hello.py
@@ -1,3 +1,4 @@
 print("Hello, my Fiend!")
 print("Я студент GeekBrains! У меня появились друзья и единомышленники в ГБ.")
 print("Через полгода мы станем разработчиками Java и Python.")
+print("Goodbue")
